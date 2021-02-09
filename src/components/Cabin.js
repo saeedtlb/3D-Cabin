c802757374f766cba88d6d224147687d4b55e33e
saeed-tlb <saeedt7879s@gmail.com>
2021-02-09 17:21:29 +0330

    load Cabin model via gltfjsx so that change diffrent mesh colors

--- src/components/Cabin.js
+++ src/components/Cabin.js
@@ -1,21 +1,38 @@
-import React, { useState, useEffect } from 'react';
-import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader';
+import React, { useEffect, useRef } from 'react';
 import { useThree } from 'react-three-fiber';
+import { useGLTF } from '@react-three/drei/core/useGLTF';
+
+import { cabin } from '../info.json';
 
 const Cabin = ({ move }) => {
-    const [model, setModel] = useState();
+    const group = useRef();
 
-    useEffect(() => {
-        new GLTFLoader().load('/models/cabin.glb', setModel);
-    }, []);
+    const { nodes, materials } = useGLTF(cabin.url);
 
-    const { camera } = useThree();
+    // const { camera } = useThree();
 
-    useEffect(() => {
-        camera.position.z = move;
-    }, [move]);
+    // useEffect(() => {
+    //     camera.position.z = move;
+    // }, [move]);
 
-    return model ? <primitive object={model.scene} /> : null;
+    return (
+        <group ref={group} position={[0.3, 2.2, 23]}>
+            <mesh
+                material={materials['BASE.005']}
+                geometry={nodes.Cube000.geometry}
+                material-color='#434343'
+            />
+            <mesh
+                material={materials.VENTANA}
+                geometry={nodes.Cube000_1.geometry}
+            />
+            <mesh
+                material={materials.sillones}
+                geometry={nodes.Cube000_2.geometry}
+                material-color='white'
+            />
+        </group>
+    );
 };
 
 export default Cabin;
