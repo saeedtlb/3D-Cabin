0e496dcf644e25c98448902e3adfb3d83359538b
saeed-tlb <saeedt7879s@gmail.com>
2021-02-09 19:39:21 +0330

    add light to scene

--- src/components/Lights.js
+++ src/components/Lights.js
@@ -3,20 +3,27 @@ import React from 'react';
 const Lights = () => {
     return (
         <>
-            <ambientLight intensity={0.1} />
-            <directionalLight position={[0, 0, 19]} />
-            <spotLight
-                position={[-10, 12, 10]}
-                penumbra={1}
+            <ambientLight intensity={0.4} />
+            <pointLight castShadow position={[11, 10.5, 8]} />
+
+            {/* <directionalLight position={[5, 10, 10]} castShadow /> */}
+
+            {/* <directionalLight
+                position={[10, 5, 13]}
+                intensity={0.3}
                 castShadow
-                intensity={0.5}
+            />
+            <directionalLight
+                position={[-10, 5, 13]}
+                intensity={0.3}
+                castShadow
             />
             <spotLight
                 position={[10, 12, 10]}
                 penumbra={1}
                 castShadow
-                intensity={0.5}
-            />
+                intensity={0.1}
+            /> */}
         </>
     );
 };
