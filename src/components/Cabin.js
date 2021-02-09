fa31dfcc3b54d203450b7fb896a3182e352093a7
saeed-tlb <saeedt7879s@gmail.com>
2021-02-09 16:26:25 +0330

    create seperate component for cabin

--- src/components/Cabin.js
+++ src/components/Cabin.js
@@ -1,17 +1,9 @@
-import { useState, useEffect, useRef } from 'react';
-// import * as THREE from 'three';
-import { useThree } from 'react-three-fiber';
+import React, { useState, useEffect } from 'react';
 import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader';
-import { useGLTF } from '@react-three/drei/core/useGLTF';
-// import { OrbitControls } from '@react-three/drei/core/OrbitControls';
+import { useThree } from 'react-three-fiber';
 
-import { seat_position } from '../info.json';
-
-const Cabin = props => {
+const Cabin = ({ move }) => {
     const [model, setModel] = useState();
-
-    const group = useRef();
-    const { nodes, materials } = useGLTF('/models/seat.glb');
 
     useEffect(() => {
         new GLTFLoader().load('/models/cabin.glb', setModel);
@@ -20,41 +12,10 @@ const Cabin = props => {
     const { camera } = useThree();
 
     useEffect(() => {
-        camera.position.z = props.move;
-    }, [props.move]);
+        camera.position.z = move;
+    }, [move]);
 
-    const renderSeats = () =>
-        seat_position.map(pos => (
-            <group position={[-0.1, -1.2, pos]} key={pos}>
-                <mesh
-                    material={materials.sillones_COLOR_0}
-                    geometry={nodes.Cube000.geometry}
-                    position={[1.6, 0.09, -0.01]}
-                    rotation={[Math.PI / 2, 0, -3.13]}
-                    material-color='sandybrown'
-                />
-                <mesh
-                    material={materials.sillones_COLOR_0}
-                    geometry={nodes.Cube001.geometry}
-                    position={[0.9, 0.06, -0.06]}
-                    rotation={[Math.PI / 2, 0, -3.13]}
-                />
-                <mesh
-                    material={materials.sillones_COLOR_0}
-                    geometry={nodes.Cube002.geometry}
-                    position={[-1.4, 0.03, -0.13]}
-                    rotation={[Math.PI / 2, 0, -3.13]}
-                />
-            </group>
-        ));
-
-    return model ? (
-        <group ref={group} dispose={null} {...props}>
-            {renderSeats()}
-            <primitive object={model.scene} />
-            {/* <OrbitControls /> */}
-        </group>
-    ) : null;
+    return model ? <primitive object={model.scene} /> : null;
 };
 
 export default Cabin;
