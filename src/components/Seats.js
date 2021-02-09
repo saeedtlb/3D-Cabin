dd35e69bd865437d97f0d43fa4ba101448d9fe43
saeed-tlb <saeedt7879s@gmail.com>
2021-02-09 17:24:00 +0330

    render seat model base on the info.json information

---
+++ src/components/Seats.js
@@ -0,0 +1,38 @@
+import React, { useRef } from 'react';
+import { useGLTF } from '@react-three/drei/core/useGLTF';
+
+import { seats } from '../info.json';
+
+const Seats = () => {
+    const group = useRef();
+    const { nodes, materials } = useGLTF(seats.url);
+
+    const renderSeats = () =>
+        seats.position.map(pos => (
+            <group ref={group} position={[-0.1, -1.2, pos]} key={pos}>
+                <mesh
+                    material={materials.sillones_COLOR_0}
+                    geometry={nodes.Cube000.geometry}
+                    position={[1.5, 0.09, -0.01]}
+                    rotation={[Math.PI / 2, 0, -3.13]}
+                    material-color='sandybrown'
+                />
+                <mesh
+                    material={materials.sillones_COLOR_0}
+                    geometry={nodes.Cube001.geometry}
+                    position={[0.8, 0.06, -0.06]}
+                    rotation={[Math.PI / 2, 0, -3.13]}
+                />
+                <mesh
+                    material={materials.sillones_COLOR_0}
+                    geometry={nodes.Cube002.geometry}
+                    position={[-1.4, 0.03, -0.13]}
+                    rotation={[Math.PI / 2, 0, -3.13]}
+                />
+            </group>
+        ));
+
+    return renderSeats();
+};
+
+export default Seats;
