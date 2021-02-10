10bbd0f0a0c38813375363cc77020793ec31d378
saeed-tlb <saeedt7879s@gmail.com>
2021-02-10 12:05:54 +0330

    add meshPhisicalMaterial and add color to it

--- src/components/Seats.js
+++ src/components/Seats.js
@@ -15,23 +15,28 @@ const Seats = () => {
                     geometry={nodes.Cube000.geometry}
                     position={[1.5, 0.09, -0.01]}
                     rotation={[Math.PI / 2, 0, -3.13]}
-                    material-color='sandybrown'
                     castShadow
-                />
+                >
+                    <meshPhysicalMaterial color='sandybrown' />
+                </mesh>
                 <mesh
                     material={materials.sillones_COLOR_0}
                     geometry={nodes.Cube001.geometry}
                     position={[0.8, 0.06, -0.06]}
                     rotation={[Math.PI / 2, 0, -3.13]}
                     castShadow
-                />
+                >
+                    <meshPhysicalMaterial color='sandybrown' />
+                </mesh>
                 <mesh
                     material={materials.sillones_COLOR_0}
                     geometry={nodes.Cube002.geometry}
                     position={[-1.4, 0.03, -0.13]}
                     rotation={[Math.PI / 2, 0, -3.13]}
                     castShadow
-                />
+                >
+                    <meshPhysicalMaterial color='sandybrown' />
+                </mesh>
             </group>
         ));
 
