8231a4d879c2a3bdf7e3c0130ba7beeceb2ea143
saeed-tlb <saeedt7879s@gmail.com>
2021-02-10 17:44:33 +0330

    remove some coments

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { Suspense, useState } from 'react';
+import React, { Suspense } from 'react';
 import { Canvas } from 'react-three-fiber';
 import * as THREE from 'three';
 
@@ -17,23 +17,6 @@ import data from './info.json';
 import './app.css';
 
 const App = () => {
-    const [zPos, setZpos] = useState(18);
-
-    const moveInVagen = ({ key }) => {
-        switch (key) {
-            case 'ArrowUp':
-            case 'w':
-                setZpos(z => (z -= 0.3));
-                break;
-            case 'ArrowDown':
-            case 's':
-                setZpos(z => (z += 0.3));
-                break;
-            default:
-                break;
-        }
-    };
-
     return (
         <Canvas
             camera={data.camera_setting}
@@ -42,13 +25,11 @@ const App = () => {
                 gl.shadowMap.enabled = true;
                 gl.shadowMap.type = THREE.PCFSoftShadowMap;
             }}
-            // tabIndex='0'
-            // onKeyDown={moveInVagen}
         >
             <Lights />
             <Plane />
             <Suspense fallback={null}>
-                <Model move={zPos} />
+                <Model />
             </Suspense>
             <OrbitControls />
         </Canvas>
