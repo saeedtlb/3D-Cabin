b62ccc295e63c8928047d1e7356f1b4430483c93
saeed-tlb <saeedt7879s@gmail.com>
2021-02-10 18:46:05 +0330

    import Alert component

--- src/App.js
+++ src/App.js
@@ -1,38 +1,39 @@
 import React, { Suspense } from 'react';
 import { Canvas } from 'react-three-fiber';
 import * as THREE from 'three';
-
 import { OrbitControls } from '@react-three/drei/core/OrbitControls';
-
+// Alert
+import Alert from './helpers/Alert';
 // Models
 import Model from './components';
 import Plane from './components/Plane';
-
 // Lights
 import Lights from './components/Lights';
-
-// Helpers
+// data
 import data from './info.json';
-
+// styles
 import './app.css';
 
 const App = () => {
     return (
-        <Canvas
-            camera={data.camera_setting}
-            shadowMap
-            onCreated={({ gl }) => {
-                gl.shadowMap.enabled = true;
-                gl.shadowMap.type = THREE.PCFSoftShadowMap;
-            }}
-        >
-            <Lights />
-            <Plane />
-            <Suspense fallback={null}>
-                <Model />
-            </Suspense>
-            <OrbitControls />
-        </Canvas>
+        <>
+            <Alert />
+            <Canvas
+                camera={data.camera_setting}
+                shadowMap
+                onCreated={({ gl }) => {
+                    gl.shadowMap.enabled = true;
+                    gl.shadowMap.type = THREE.PCFSoftShadowMap;
+                }}
+            >
+                <Lights />
+                <Plane />
+                <Suspense fallback={null}>
+                    <Model />
+                </Suspense>
+                <OrbitControls />
+            </Canvas>
+        </>
     );
 };
 
