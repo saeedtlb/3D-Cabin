aea7c41f2ddfcb58e5f219914c94e5d56b843f75
saeed-tlb <saeedt7879s@gmail.com>
2021-02-10 12:05:00 +0330

    add suitable lights to scene

--- src/components/Lights.js
+++ src/components/Lights.js
@@ -4,26 +4,8 @@ const Lights = () => {
     return (
         <>
             <ambientLight intensity={0.4} />
-            <pointLight castShadow position={[11, 10.5, 8]} />
 
-            {/* <directionalLight position={[5, 10, 10]} castShadow /> */}
-
-            {/* <directionalLight
-                position={[10, 5, 13]}
-                intensity={0.3}
-                castShadow
-            />
-            <directionalLight
-                position={[-10, 5, 13]}
-                intensity={0.3}
-                castShadow
-            />
-            <spotLight
-                position={[10, 12, 10]}
-                penumbra={1}
-                castShadow
-                intensity={0.1}
-            /> */}
+            <spotLight position={[11, 10.5, 13]} castShadow penumbra={1} />
         </>
     );
 };
