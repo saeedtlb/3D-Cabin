df5d164626c9724f715c430462e17202443793bc
saeed-tlb <saeedt7879s@gmail.com>
2021-02-11 19:21:51 +0330

    add values to our store to be able use them and update them later

--- src/context/store.js
+++ src/context/store.js
@@ -1,4 +1,7 @@
 export default {
-    soldOut: [],
-    user_seat: [],
+    soldOuts: ['2C', '5A', '8B'],
+    // user_seat: [],
+    user_seat: new Set(),
+    show_message: false,
+    styles: null,
 };
