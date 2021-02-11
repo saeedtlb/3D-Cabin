23dab809a7eb2b5f80dc3bad1de9a90289199bc1
saeed-tlb <saeedt7879s@gmail.com>
2021-02-11 19:27:35 +0330

    show alert message base on store value(it's successfull | warn | wrong) for 3.5s and then hide it.

--- src/helpers/Alert.js
+++ src/helpers/Alert.js
@@ -1,12 +1,28 @@
-import React from 'react';
+import React, { useEffect, useRef } from 'react';
+
+import { useGlobalState, useGlobalDispatch } from '../context/globalContext';
+
+import { hideMessage } from '../actions/action';
 
 const Alert = () => {
+    const { seat_num, show_message, styles } = useGlobalState(); // if we get styles it means that seat has alredy sold
+    const dispatch = useGlobalDispatch();
+    const alert = useRef();
+
+    useEffect(() => {
+        show_message && setTimeout(() => dispatch(hideMessage()), 3500);
+    }, [show_message]);
+
     return (
-        <div className='alert'>
-            <div className='head'>Successful</div>
+        <div
+            className={show_message ? 'alert alert_show' : 'alert'}
+            ref={alert}
+            style={styles && styles}
+        >
+            <div className='head'>{styles ? 'sorry!!!' : 'Successful'}</div>
             <div className='main'>
-                Register seat number{' '}
-                <span style={{ fontWeight: 'bold' }}>4B</span>
+                {styles ? 'Already sold' : 'Register'} seat number
+                <span style={{ fontWeight: 'bold' }}> {seat_num}</span>
             </div>
         </div>
     );
