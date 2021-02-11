79fb33afbf618d8b94860d72cc473bcc0ca53cab
saeed-tlb <saeedt7879s@gmail.com>
2021-02-11 19:25:44 +0330

    detect which seat has clicked and change the color correctly and also update the store

--- src/components/Seats.js
+++ src/components/Seats.js
@@ -1,36 +1,82 @@
 import React, { useRef } from 'react';
 import { useGLTF } from '@react-three/drei/core/useGLTF';
+// app data
 import { seats } from '../info.json';
+// action
+import { selectSeat } from '../actions/action';
+// types
+import { USER, AVAILABLE, SOLD } from '../actions/type';
 
-const Seats = () => {
+const Seats = ({ store, dispatch }) => {
+    const { soldOuts, user_seat } = store;
+
     const group = useRef();
     const { nodes, materials } = useGLTF(seats.url);
+
+    const checkSeatStatus = num => {
+        // const is_user_seat = user_seat.indexOf(num) > -1 ? true : false;
+        const is_user_seat = user_seat.has(num);
+
+        if (is_user_seat) return USER;
+
+        const is_available = soldOuts.indexOf(num) === -1 ? true : false;
+
+        return is_available ? AVAILABLE : SOLD;
+    };
 
     const seatMesh = row_num =>
         Object.values(nodes)
             .slice(1)
-            .map((node, i) => (
-                <mesh
-                    key={row_num + seats.numbers[i]}
-                    name={row_num + seats.numbers[i]}
-                    position={seats.position[i]}
-                    geometry={node.geometry}
-                    material={materials.sillones_COLOR_0}
-                    rotation={[Math.PI / 2, 0, -3.13]}
-                    castShadow
-                >
-                    <meshPhysicalMaterial color='#ED081A' />
-                </mesh>
-            ));
+            .map((node, i) => {
+                const seat_num = row_num + seats.numbers[i];
+                const status = checkSeatStatus(seat_num);
+                let color = null;
 
-    const changeColor = ({ stopPropagation, object }) => {
-        console.log(object.name);
+                switch (status) {
+                    case AVAILABLE:
+                        color = '#7ADA48';
+                        break;
+                    case USER:
+                        color = 'sandybrown';
+                        break;
+                    default:
+                        color = '#ED081A';
+                        break;
+                }
+
+                return (
+                    <mesh
+                        key={seat_num}
+                        name={seat_num}
+                        position={seats.position[i]}
+                        geometry={node.geometry}
+                        material={materials.sillones_COLOR_0}
+                        rotation={[Math.PI / 2, 0, -3.13]}
+                        castShadow
+                    >
+                        <meshPhysicalMaterial color={color} />
+                    </mesh>
+                );
+            });
+
+    const selectedSeat = ({ stopPropagation, object }) => {
         stopPropagation();
-        object.material.color = {
-            r: 0,
-            g: 1,
-            b: 0,
-        };
+        const { name: seat_num } = object;
+        const status = checkSeatStatus(seat_num);
+
+        console.log(22, status);
+
+        switch (status) {
+            case AVAILABLE:
+                dispatch(selectSeat(seat_num));
+                break;
+            case USER:
+                dispatch(selectSeat(seat_num, USER));
+                break;
+            default:
+                dispatch(selectSeat(seat_num, SOLD));
+                break;
+        }
     };
 
     const renderSeats = () =>
@@ -39,7 +85,7 @@ const Seats = () => {
                 ref={group}
                 position={[-0.1, -1.2, pos]}
                 key={pos}
-                onPointerDown={changeColor}
+                onPointerDown={selectedSeat}
             >
                 {seatMesh(i + 1)}
             </group>
