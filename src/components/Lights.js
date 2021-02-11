f24167ea062bc6d611a05b04b647bd5daf084898
saeed-tlb <saeedt7879s@gmail.com>
2021-02-11 19:20:10 +0330

    since we just return jsx from this component delete remove word and wrap jsx into ()

--- src/components/Lights.js
+++ src/components/Lights.js
@@ -1,12 +1,10 @@
 import React from 'react';
 
-const Lights = () => {
-    return (
-        <>
-            <ambientLight intensity={0.4} />
-            <spotLight position={[11, 10.5, 13]} castShadow penumbra={1} />
-        </>
-    );
-};
+const Lights = () => (
+    <>
+        <ambientLight intensity={0.4} />
+        <spotLight position={[11, 10.5, 13]} castShadow penumbra={1} />
+    </>
+);
 
 export default Lights;
