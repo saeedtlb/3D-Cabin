e6555965343aafc10fc36c07b3bedcc2c7b4b3f2
saeed-tlb <saeedt7879s@gmail.com>
2021-02-12 16:06:45 +0330

    a component which show the column name in the cabin

---
+++ src/helpers/Tuturial.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Tuturial = () => {
+    return (
+        <div className='tuturial'>
+            <div>hi</div>
+        </div>
+    );
+};
+
+export default Tuturial;
