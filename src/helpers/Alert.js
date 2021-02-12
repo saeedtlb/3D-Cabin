2176ab58a06a874b113c6d885949746bdd955926
saeed-tlb <saeedt7879s@gmail.com>
2021-02-12 15:34:47 +0330

    show alert to inform user

--- src/helpers/Alert.js
+++ src/helpers/Alert.js
@@ -5,23 +5,23 @@ import { useGlobalState, useGlobalDispatch } from '../context/globalContext';
 import { hideMessage } from '../actions/action';
 
 const Alert = () => {
-    const { seat_num, show_message, styles } = useGlobalState(); // if we get styles it means that seat has alredy sold
+    const { seat_num, show_message, styles } = useGlobalState();
     const dispatch = useGlobalDispatch();
     const alert = useRef();
 
     useEffect(() => {
-        show_message && setTimeout(() => dispatch(hideMessage()), 3500);
+        show_message.status && setTimeout(() => dispatch(hideMessage()), 3500);
     }, [show_message]);
 
     return (
         <div
-            className={show_message ? 'alert alert_show' : 'alert'}
+            className={show_message.status ? 'alert alert_show' : 'alert'}
             ref={alert}
             style={styles && styles}
         >
-            <div className='head'>{styles ? 'sorry!!!' : 'Successful'}</div>
+            <div className='head'>{show_message.head}</div>
             <div className='main'>
-                {styles ? 'Already sold' : 'Register'} seat number
+                {show_message.body} seat number
                 <span style={{ fontWeight: 'bold' }}> {seat_num}</span>
             </div>
         </div>
