031765855a80f9d37fb04240f140fc2a56e004ab
saeed-tlb <saeedt7879s@gmail.com>
2021-02-13 18:37:58 +0330

    import tuturial component to main entry file and use it there

--- src/App.js
+++ src/App.js
@@ -1,48 +1,49 @@
-import React, { Suspense, useState } from 'react';
-import { Canvas } from 'react-three-fiber';
-import * as THREE from 'three';
-import { OrbitControls } from '@react-three/drei/core/OrbitControls';
+import React, { Suspense, useState } from 'react'
+import { Canvas } from 'react-three-fiber'
+import * as THREE from 'three'
+import { OrbitControls } from '@react-three/drei/core/OrbitControls'
 // Side components
-import Alert from './helpers/Alert';
-import Tuturial from './helpers/Tuturial';
+import Alert from './helpers/Alert'
+import Tuturial from './helpers/Tuturial'
 // Models
-import Model from './components';
-import Plane from './components/Plane';
+import Model from './components'
+import Plane from './components/Plane'
 // Lights
-import Lights from './components/Lights';
+import Lights from './components/Lights'
 // data
-import data from './info.json';
+import data from './info.json'
 // styles
-import './app.css';
+import './app.css'
 // Provider to be able use store in Canvas Elements
-import { useGlobalDispatch, useGlobalState } from './context/globalContext';
+import { useGlobalDispatch, useGlobalState } from './context/globalContext'
 
 const App = () => {
-    const [showTuturial, setShowTuturial] = useState(true);
-    const store = useGlobalState();
-    const dispatch = useGlobalDispatch();
+  const [showTuturial, setShowTuturial] = useState(true)
+  const store = useGlobalState()
+  const dispatch = useGlobalDispatch()
 
-    return (
-        <>
-            {showTuturial ? <Tuturial /> : null}
-            <Alert />
-            <Canvas
-                camera={data.camera_setting}
-                shadowMap
-                onCreated={({ gl }) => {
-                    gl.shadowMap.enabled = true;
-                    gl.shadowMap.type = THREE.PCFSoftShadowMap;
-                }}
-            >
-                <Lights />
-                <Plane />
-                <Suspense fallback={null}>
-                    <Model store={store} dispatch={dispatch} />
-                </Suspense>
-                <OrbitControls />
-            </Canvas>
-        </>
-    );
-};
+  const stopShowTuturial = () => setShowTuturial(false)
 
-export default App;
+  return (
+    <>
+      {showTuturial ? <Tuturial stopShowTuturial={stopShowTuturial} /> : null}
+      <Alert />
+      <Canvas
+        camera={data.camera_setting}
+        shadowMap
+        onCreated={({ gl }) => {
+          gl.shadowMap.enabled = true
+          gl.shadowMap.type = THREE.PCFSoftShadowMap
+        }}>
+        <Lights />
+        <Plane />
+        <Suspense fallback={null}>
+          <Model store={store} dispatch={dispatch} />
+        </Suspense>
+        <OrbitControls />
+      </Canvas>
+    </>
+  )
+}
+
+export default App
