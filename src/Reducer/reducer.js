c0d02c9091d810c14524c33789ae08d384ea2086
saeed-tlb <saeedt7879s@gmail.com>
2021-02-13 18:38:25 +0330

    assign reducer to a varriable and export the var as default

--- src/Reducer/reducer.js
+++ src/Reducer/reducer.js
@@ -1,52 +1,54 @@
-import { HIDE_MESSAGE, AVAILABLE, SOLD, USER } from '../actions/type';
+import { HIDE_MESSAGE, AVAILABLE, SOLD, USER } from '../actions/type'
 
-export default (state, action) => {
-    console.log(action);
+const globalReducer = (state, action) => {
+  console.log(action)
 
-    switch (action.type) {
-        case AVAILABLE:
-            return {
-                ...state,
-                seat_num: action.seat_num,
-                user_seat: state.user_seat.add(action.seat_num),
-                show_message: {
-                    status: true,
-                    head: 'Successful',
-                    body: 'Register',
-                },
-                styles: null,
-            };
-        case USER:
-            state.user_seat.delete(action.payload.seat_num);
-            return {
-                ...state,
-                seat_num: action.payload.seat_num,
-                styles: action.payload.styles,
-                show_message: {
-                    status: true,
-                    head: 'Remove',
-                    body: 'Remove your',
-                },
-            };
-        case SOLD:
-            return {
-                ...state,
-                seat_num: action.payload.seat_num,
-                styles: action.payload.styles,
-                show_message: {
-                    status: true,
-                    head: 'Sorry!!!',
-                    body: 'Already sold',
-                },
-            };
-        case HIDE_MESSAGE:
-            return {
-                ...state,
-                show_message: {
-                    status: false,
-                },
-            };
-        default:
-            console.log('somthing went wrong');
-    }
-};
+  switch (action.type) {
+    case AVAILABLE:
+      return {
+        ...state,
+        seat_num: action.seat_num,
+        user_seat: state.user_seat.add(action.seat_num),
+        show_message: {
+          status: true,
+          head: 'Successful',
+          body: 'Register'
+        },
+        styles: null
+      }
+    case USER:
+      state.user_seat.delete(action.payload.seat_num)
+      return {
+        ...state,
+        seat_num: action.payload.seat_num,
+        styles: action.payload.styles,
+        show_message: {
+          status: true,
+          head: 'Remove',
+          body: 'Remove your'
+        }
+      }
+    case SOLD:
+      return {
+        ...state,
+        seat_num: action.payload.seat_num,
+        styles: action.payload.styles,
+        show_message: {
+          status: true,
+          head: 'Sorry!!!',
+          body: 'Already sold'
+        }
+      }
+    case HIDE_MESSAGE:
+      return {
+        ...state,
+        show_message: {
+          status: false
+        }
+      }
+    default:
+      console.log('somthing went wrong')
+  }
+}
+
+export default globalReducer
