09ed5141941df4099fad9fa76e781e5742d3e0b5
saeed-tlb <saeedt7879s@gmail.com>
2021-02-14 18:54:49 +0330

    add texture to the plane

--- src/components/Plane.js
+++ src/components/Plane.js
@@ -1,8 +1,20 @@
-const Plane = () => (
-    <mesh rotation={[-Math.PI / 2, 0, 0]} position={[0, -1.5, 0]} receiveShadow>
-        <planeBufferGeometry attach='geometry' args={[2, 40]} />
-        <meshPhysicalMaterial attach='material' color='pink' />
-    </mesh>
-);
+import React from 'react'
+import { useTexture } from '@react-three/drei/core/useTexture'
+import * as THREE from 'three'
+// texture
+import { Asphalt_texture } from '../info.json'
 
-export default Plane;
+const Plane = () => {
+  const asphalt = useTexture(Asphalt_texture)
+  asphalt.wrapS = asphalt.wrapT = THREE.RepeatWrapping
+  asphalt.magFilter = asphalt.minFilter = THREE.LinearFilter
+
+  return (
+    <mesh rotation={[-Math.PI / 2, 0, 0]} position={[0, -1.5, 0]} receiveShadow>
+      <planeBufferGeometry attach="geometry" args={[10, 55]} />
+      <meshBasicMaterial attach="material" map={asphalt} side={THREE.DoubleSide} />
+    </mesh>
+  )
+}
+
+export default Plane
