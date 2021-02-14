b52a13d595fd68d5d3aadb435f32b3bb88ed3512
saeed-tlb <saeedt7879s@gmail.com>
2021-02-14 18:56:09 +0330

    assign the store obj to a var and export it as default

--- src/context/store.js
+++ src/context/store.js
@@ -1,6 +1,8 @@
-export default {
-    soldOuts: ['2C', '5A', '8B'],
-    user_seat: new Set(),
-    show_message: false,
-    styles: null,
-};
+const initialStore = {
+  soldOuts: ['2C', '5A', '8B'],
+  user_seat: new Set(),
+  show_message: false,
+  styles: null
+}
+
+export default initialStore
