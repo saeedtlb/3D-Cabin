4b4dfe7e4737b8d750461679a1842f75f4c5fda5
saeed-tlb <saeedt7879s@gmail.com>
2021-02-14 18:57:02 +0330

    add dispatch to dependency array of useEffect

--- src/helpers/Alert.js
+++ src/helpers/Alert.js
@@ -1,31 +1,27 @@
-import React, { useEffect, useRef } from 'react';
+import React, { useEffect, useRef } from 'react'
 
-import { useGlobalState, useGlobalDispatch } from '../context/globalContext';
+import { useGlobalState, useGlobalDispatch } from '../context/globalContext'
 
-import { hideMessage } from '../actions/action';
+import { hideMessage } from '../actions/action'
 
 const Alert = () => {
-    const { seat_num, show_message, styles } = useGlobalState();
-    const dispatch = useGlobalDispatch();
-    const alert = useRef();
+  const { seat_num, show_message, styles } = useGlobalState()
+  const dispatch = useGlobalDispatch()
+  const alert = useRef()
 
-    useEffect(() => {
-        show_message.status && setTimeout(() => dispatch(hideMessage()), 3500);
-    }, [show_message]);
+  useEffect(() => {
+    show_message.status && setTimeout(() => dispatch(hideMessage()), 3500)
+  }, [show_message, dispatch])
 
-    return (
-        <div
-            className={show_message.status ? 'alert alert_show' : 'alert'}
-            ref={alert}
-            style={styles && styles}
-        >
-            <div className='head'>{show_message.head}</div>
-            <div className='main'>
-                {show_message.body} seat number
-                <span style={{ fontWeight: 'bold' }}> {seat_num}</span>
-            </div>
-        </div>
-    );
-};
+  return (
+    <div className={show_message.status ? 'alert alert_show' : 'alert'} ref={alert} style={styles && styles}>
+      <div className="head">{show_message.head}</div>
+      <div className="main">
+        {show_message.body} seat number
+        <span style={{ fontWeight: 'bold' }}> {seat_num}</span>
+      </div>
+    </div>
+  )
+}
 
-export default Alert;
+export default Alert
